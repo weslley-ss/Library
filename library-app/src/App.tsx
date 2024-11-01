9f8b9e40c3feab98f03eb999e9d6710a9b9f5d73
weslley-ss <weslle.ws@gmail.com>
2024-11-01 02:18:59 -0300

    Build: initial setup

---
+++ library-app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App bg bg-light">
+      <h1> Hello React World! <i className="bi bi-globe-americas"></i></h1>
+    </div> 
+  );
+}
+export default App;
